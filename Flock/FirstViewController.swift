0b92c42aa86cb37122f1e2bd2657d6d7b225eced
melvinaa <melvinaduadjei@gmail.com>
2017-05-27 15:12:25 -0400

    Initial Commit

---
+++ Flock/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flock
+//
+//  Created by Vin on 2017-05-27.
+//  Copyright © 2017 Vin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
